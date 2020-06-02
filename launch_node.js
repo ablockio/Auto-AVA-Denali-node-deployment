96a8028cee7856f71ed67a485a22bc5d441a27df
aorfevre <aorfevre@gmail.com>
2020-06-02 15:43:28 +0200

    Update wording & README

--- launch_node.js
+++ launch_node.js
@@ -20,26 +20,25 @@ var d = {
 }
 
 
-console.log("  ______   _______   __         ______    ______   __    __ ");
-console.log(" /      \ /       \ /  |       /      \  /      \ /  |  /  |");
-console.log("/$$$$$$  |$$$$$$$  |$$ |      /$$$$$$  |/$$$$$$  |$$ | /$$/ ");
-console.log("$$ |__$$ |$$ |__$$ |$$ |      $$ |  $$ |$$ |  $$/ $$ |/$$/  ");
-console.log("$$    $$ |$$    $$< $$ |      $$ |  $$ |$$ |      $$  $$<   ");
-console.log("$$$$$$$$ |$$$$$$$  |$$ |      $$ |  $$ |$$ |   __ $$$$$  \  ");
-console.log("$$ |  $$ |$$ |__$$ |$$ |_____ $$ \__$$ |$$ \__/  |$$ |$$  \ ");
-console.log("$$ |  $$ |$$    $$/ $$       |$$    $$/ $$    $$/ $$ | $$  |");
-console.log("$$/   $$/ $$$$$$$/  $$$$$$$$/  $$$$$$/   $$$$$$/  $$/   $$/ ");
+console.log("  /$$$$$$  /$$    /$$  /$$$$$$                      /$$$$$$$  /$$$$$$$$ /$$   /$$  /$$$$$$  /$$       /$$$$$$");
+console.log(" /$$__  $$| $$   | $$ /$$__  $$                    | $$__  $$| $$_____/| $$$ | $$ /$$__  $$| $$      |_  $$_/");
+console.log("| $$  \ $$| $$   | $$| $$  \ $$                    | $$  \ $$| $$      | $$$$| $$| $$  \ $$| $$        | $$  ");
+console.log("| $$$$$$$$|  $$ / $$/| $$$$$$$$       /$$$$$$      | $$  | $$| $$$$$   | $$ $$ $$| $$$$$$$$| $$        | $$  ");
+console.log("| $$__  $$ \  $$ $$/ | $$__  $$      |______/      | $$  | $$| $$__/   | $$  $$$$| $$__  $$| $$        | $$  ");
+console.log("| $$  | $$  \  $$$/  | $$  | $$                    | $$  | $$| $$      | $$\  $$$| $$  | $$| $$        | $$  ");
+console.log("| $$  | $$   \  $/   | $$  | $$                    | $$$$$$$/| $$$$$$$$| $$ \  $$| $$  | $$| $$$$$$$$ /$$$$$$");
+console.log("|__/  |__/    \_/    |__/  |__/                    |_______/ |________/|__script powered by https://ablock.io");
 
 
 
 
 async function start() {
 
-  console.log('1.Stake your AVA')
-  console.log('Create your KEYSTORE')
+
+  console.log('## Creating your KEYSTORE ...')
 
   let keyStoreResponse = await helper.createKeystore();
-  console.log('Keystore is created', keyStoreResponse.body)
+
   d.keyStoreResponse = keyStoreResponse.body
   // Check if success OR if already created
   if ((d.keyStoreResponse.result !== undefined && d.keyStoreResponse.result.success !== undefined && d.keyStoreResponse.result.success === true) ||
@@ -54,22 +53,24 @@ async function start() {
 }
 
 async function createWallet() {
+  console.log('## Creating your X-Chain wallet ...')
+
   let xWalletResponse = await helper.createAddressXChain();
-  console.log('xWalletResponse', xWalletResponse.statusCode)
 
   if (xWalletResponse.statusCode === 404) {
 
 
-    console.log("Your node is probably not synced, wallet is not created")
-    console.log("we will retry in 60sec.")
+    console.log("ERROR : Your node is probably not synced, wallet is not created. ")
+    console.log('The script will retry in 60 sec automaticly ...')
+
     await new Promise(resolve => setTimeout(resolve, 60000));
-    console.log('Trying to launch the node again')
+    console.log('Trying to create your wallet again ...')
 
     createWallet()
 
   } else {
     d.xWalletResponse = xWalletResponse.body
-    console.log('Wallet is created is created on X-Chain', d.xWalletResponse.result.address)
+    console.log('X-Chain wallet created: ' + d.xWalletResponse.result.address)
     nodeIsLaunchedProcess()
   }
 
@@ -79,31 +80,32 @@ async function createWallet() {
 }
 
 async function nodeIsLaunchedProcess() {
+  console.log('## Creating your P-Chain wallet ...')
   let pWalletResponse = await helper.createAddressPChain();
   d.pWalletResponse = pWalletResponse.body
 
-  console.log('Wallet is created is created on P-Chain', d.pWalletResponse.result.address)
-
+  console.log('X-Chain wallet created: ' + d.pWalletResponse.result.address)
 
   claimFaucet()
 }
 
 async function claimFaucet() {
-  console.log('*******');
-  console.log('Go to https://faucet.ava.network/ and claim tokens');
+  console.log('## Provisionning nAVA tokens ...')
+  console.log('Connect to https://faucet.ava.network/ and claim tokens');
   console.log('Type your X-Chain wallet ', d.xWalletResponse.result.address)
   console.log('When you have successfully received your tokens, press any key to continue');
 
-  await keypress()
+  // await keypress()
 
   var balance = await helper.checkWalletBalanceXChain(d.xWalletResponse.result.address)
-  console.log("Balance", balance.body)
+
   if (balance.body.result.balance > 10000) {
     console.log('Your balance has ' + balance.body.result.balance + ' nAVA');
     sendAVAtoPChain()
   } else {
 
-    console.log('You do not have enough nAVA, claim some tokens');
+    console.log('You do not have enough nAVA. Checking again in 30 sec automaticly');
+    await new Promise(resolve => setTimeout(resolve, 30000));
     claimFaucet()
 
   }
@@ -111,11 +113,8 @@ async function claimFaucet() {
 }
 
 async function sendAVAtoPChain() {
-  console.log('Exporting tokens from X Chain to P Chain', d.pWalletResponse.result.address)
+  console.log('## Exporting tokens from X-Chain to P-Chain', d.pWalletResponse.result.address)
   var exportVal = await helper.exportAVAXChainToPChain(d.pWalletResponse.result.address)
-  console.log('Accepting transfer to pchain', exportVal.body)
-
-
 
 
   checkStatus(exportVal);
@@ -129,25 +128,23 @@ async function sendAVAtoPChain() {
   //
 }
 async function checkStatus(exportVal) {
-  console.log("Waiting 5 sec", d.pWalletResponse.result.address)
-  await new Promise(resolve => setTimeout(resolve, 5000));
+  // console.log("Waiting 5 sec", d.pWalletResponse.result.address)
+  // await new Promise(resolve => setTimeout(resolve, 5000));
   var txStatus = await helper.getTxStatusX(exportVal.body.result.txID)
-  console.log('txStatus', txStatus.body)
+  // console.log('txStatus', txStatus.body)
 
   if (txStatus.body.result.status === 'Processing') {
+    console.log("Transaction is not yet confirmed, waiting 5 sec");
+    await new Promise(resolve => setTimeout(resolve, 5000));
     checkStatus(exportVal);
   } else {
 
-    console.log("Waiting 5 sec", d.pWalletResponse.result.address)
-    await new Promise(resolve => setTimeout(resolve, 5000));
+
     var tx = await helper.acceptTransferPChain(d.pWalletResponse.result.address)
 
-    console.log("Waiting 5 sec", d.pWalletResponse.result.address)
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    console.log('Issuing tx to pchain : ', tx.body.result.tx)
+    // await new Promise(resolve => setTimeout(resolve, 5000));
+    console.log('##  Issuing transation to P-Chain : ', tx.body.result.tx)
     let issue = await helper.issueTxPChain(tx.body.result.tx)
-    console.log("Issue body", issue.body)
-
 
     checkBalancePchain()
 
@@ -156,14 +153,17 @@ async function checkStatus(exportVal) {
 }
 
 async function checkBalancePchain() {
-  console.log("Waiting 5 sec", d.pWalletResponse.result.address)
-  await new Promise(resolve => setTimeout(resolve, 5000));
+
   let balancePChain = await helper.getAccountPChain(d.pWalletResponse.result.address)
-  console.log("Balance pChain: ", balancePChain.body)
+
 
   if (Number(balancePChain.body.result.balance) === 0) {
+
+    console.log('Balance is still 0, waiting 5 sec')
+    await new Promise(resolve => setTimeout(resolve, 5000));
     checkBalancePchain()
   } else {
+    console.log("Your balance on P-Chain is : ", balancePChain.body)
     validation()
   }
 
@@ -171,26 +171,35 @@ async function checkBalancePchain() {
 async function validation() {
   let node = await helper.validation();
 
+  console.log('##  Getting node ID...')
+
   d.node = node.body;
-  console.log("Node", d.node)
+  console.log('Node Id:', d.node.result.nodeID)
+  console.log('##  Creating unsigned transaction...')
+
   let unsigned = await helper.unsignedNodeTx(d.node.result.nodeID, d.pWalletResponse.result.address)
-  console.log("unsigned", unsigned.body)
+  console.log('##  Signing unsigned transaction...')
   let signed = await helper.signTxPChain(unsigned.body.result.unsignedTx, d.pWalletResponse.result.address)
-
-  console.log('signed', signed.body)
+  console.log('##  Issuing Transaction to P-Chain...')
   let issuePchain = await helper.issueTxPChainAdmin(signed.body.result.tx)
-  console.log('issue', issuePchain.body)
-  console.log("  ______   _______   __         ______    ______   __    __ ");
-  console.log(" /      \ /       \ /  |       /      \  /      \ /  |  /  |");
-  console.log("/$$$$$$  |$$$$$$$  |$$ |      /$$$$$$  |/$$$$$$  |$$ | /$$/ ");
-  console.log("$$ |__$$ |$$ |__$$ |$$ |      $$ |  $$ |$$ |  $$/ $$ |/$$/  ");
-  console.log("$$    $$ |$$    $$< $$ |      $$ |  $$ |$$ |      $$  $$<   ");
-  console.log("$$$$$$$$ |$$$$$$$  |$$ |      $$ |  $$ |$$ |   __ $$$$$  \  ");
-  console.log("$$ |  $$ |$$ |__$$ |$$ |_____ $$ \__$$ |$$ \__/  |$$ |$$  \ ");
-  console.log("$$ |  $$ |$$    $$/ $$       |$$    $$/ $$    $$/ $$ | $$  |");
-  console.log("$$/   $$/ $$$$$$$/  $$$$$$$$/  $$$$$$/   $$$$$$/  $$/   $$/ ");
 
-  console.log("BOOOM YOUR ARE DONE. YOU SHALL BE IN THE LIST OF VALIDATORS")
+
+  console.log("  /$$$$$$  /$$    /$$  /$$$$$$                      /$$$$$$$  /$$$$$$$$ /$$   /$$  /$$$$$$  /$$       /$$$$$$");
+  console.log(" /$$__  $$| $$   | $$ /$$__  $$                    | $$__  $$| $$_____/| $$$ | $$ /$$__  $$| $$      |_  $$_/");
+  console.log("| $$  \ $$| $$   | $$| $$  \ $$                    | $$  \ $$| $$      | $$$$| $$| $$  \ $$| $$        | $$  ");
+  console.log("| $$$$$$$$|  $$ / $$/| $$$$$$$$       /$$$$$$      | $$  | $$| $$$$$   | $$ $$ $$| $$$$$$$$| $$        | $$  ");
+  console.log("| $$__  $$ \  $$ $$/ | $$__  $$      |______/      | $$  | $$| $$__/   | $$  $$$$| $$__  $$| $$        | $$  ");
+  console.log("| $$  | $$  \  $$$/  | $$  | $$                    | $$  | $$| $$      | $$\  $$$| $$  | $$| $$        | $$  ");
+  console.log("| $$  | $$   \  $/   | $$  | $$                    | $$$$$$$/| $$$$$$$$| $$ \  $$| $$  | $$| $$$$$$$$ /$$$$$$");
+  console.log("|__/  |__/    \_/    |__/  |__/                    |_______/ |________/|__script powered by https://ablock.io");
+  console.log("#############################################################################################################");
+
+
+  console.log("Boom! You are done. Check your validator address ")
   console.log("https://explorer.ava.network/validators")
+  console.log("Recap ")
+  console.log("X-Chain Wallet: " + d.xWalletResponse.result.address)
+  console.log("P-Chain Wallet: " + d.pWalletResponse.result.address)
+  console.log("Node ID: " + d.node.result.nodeID)
 }
 start()
